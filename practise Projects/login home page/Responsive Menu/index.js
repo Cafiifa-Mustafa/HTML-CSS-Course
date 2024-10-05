43a914f552a2a2d5a962eb02aaec3f1847161aa7
cafiifa-mustafa <cafiifahariir@gmail.com>
2024-10-05 16:16:44 +0300

    add changes

---
+++ practise Projects/login home page/Responsive Menu/index.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+  const navLinks = document.getElementById("navLinks");
+  const burger = document.getElementById("burger");
+
+  navLinks.classList.toggle("active");
+  burger.classList.toggle("toggle");
+} 
